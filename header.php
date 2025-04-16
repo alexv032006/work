a2315c312473cf31ff7d1617bff8bb819c68c0d1
Alex <sashuha06@gmail.com>
2025-04-16 12:59:53 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+if (!isset($_SESSION)) session_start();
+
+
+$tabs = [
+    'Главная' => 'main.php',
+    'Профиль' => '#',
+    'Контакты' => '#',
+    'Админ-панель' => '#',
+    'Настройки' => '#'
+];
+
+$visible = $role === 'admin' ? $tabs : array_slice($tabs, 0, 3);
+
+echo "<div style='background:#222;padding:10px;'>";
+foreach ($visible as $title => $url) {
+    echo "<a href='$url' style='color:white; margin-right:15px;'>$title</a>";
+}
+echo "<a href='logout.php' style='color:orange; float:right;'>Выйти</a>";
+echo "</div><br>";
+?>
